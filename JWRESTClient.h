dc1ec7e05a97c6ae3271d5654d0904dd7236cd4d
Julian Weinert <julian@weinert.me>
2014-05-06 09:39:07 +0200

    Update JWRESTClient.h

--- JWRESTClient.h
+++ JWRESTClient.h
@@ -2,6 +2,7 @@
 //  JWRESTClient.h
 //
 //  Created by Julian Weinert on 06.06.13.
+//  Originally created for http://www.csundm.com
 //  Copyright (c) 2013 Julian Weinert.
 //
 //    This program is free software: you can redistribute it and/or modify
